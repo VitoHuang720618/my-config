ebe41ca8409b0af85b6d389a15f52ce019c0d767
Vito <vito@CY0018952-Vito.local>
2024-01-18 16:27:18 +0800

    add nvim wezterm

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = Util.safe_keymap_set
+
+-- map("n", "<C-e>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- map("n", "<leader>d", "<cmd>bdelete<cr>", { desc = "delete buffer" })
+
+map("n", "<leader>d", ":bp|bd #<CR>", { desc = "delete buffer" })
+
+-- Resize window
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
